82cdd0818cfd9a4f9a9258cdb7f3965cfba5e0f7
Jonathan Yu <Jonathan.Yu2@optus.com.au>
2021-05-21 10:22:08 +1000

    Implemented function: find_nearest_match

--- matcher.py
+++ matcher.py
@@ -10,36 +10,47 @@ Created on Tue May 18 23:00:28 2021
 
 # %% Import libraries
 import numpy as np
+import scipy as sp
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # %% Matcher
 class Matcher:
+    """An object for performing company name matching."""
     
-    def __init__(self, corpus):
+    def __init__(self, company_names):
+        """Initialise Matcher"""
         self.vectoriser = TfidfVectorizer()
-        self.vectoriser.fit(corpus)
-    
-    def fit(self, corpus):
-        self.vectoriser.fit(corpus)
-        
-    def transform(self, company):
-        assert(type(company)==str)
-        return self.vectoriser.transform([company]).toarray()
-    
-    def transform_corpus(self, corpus):
-        assert(type(corpus)==list)
-        return self.vectoriser.transform(corpus).toarray()
+        self.vectoriser.fit(company_names)
 
-    def find_nearest_match(self, company_v, corpus_v, n=1):
-        assert(corpus_v.shape[1]==company_v.shape[1])
-        dist_v = np.linalg.norm(corpus_v-company_v, axis=1)
-        match_idx_l = np.argsort(dist_v)
-        match_l = [(idx,dist_v[idx]) for idx in match_idx_l[:n]]
+    def _convert_vec_to_mat(self, vec, mat):
+        """Convert 1xN vector into MxN matrix."""
+        data = np.tile(vec.data,16419)
+        indices = np.tile(vec.indices,16419) 
+        indptr = np.array([i*vec.indptr[1] for i in range(16420)])
+        vec_mat = sp.sparse.csr_matrix((data,indices,indptr), shape=mat.shape)
+        return vec_mat        
+        
+    def fit(self, company_names):
+        """Fit matcher to a list of company names."""
+        return self.vectoriser.fit(company_names)
+        
+    def transform_one(self, company_name):
+        """Return sparse vector representation of company name."""
+        return self.vectoriser.transform([company_name])
+    
+    def transform_many(self, company_names):
+        """Return sparse matrix from a list of company names."""
+        return self.vectoriser.transform(company_names)
+
+    def find_nearest_match(self, vec, mat, threshold=0.9, n=1):
+        """Return N nearest matches between sparse vector and sparse matrix."""
+        vec_mat = self._convert_vec_to_mat(vec,mat) # Convert 1xN vector into MxN matrix
+        dist = sp.sparse.linalg.norm(mat-vec_mat,axis=1)
+        match_idx = np.argsort(dist)
+        match_l = [(idx,dist[idx]) for idx in match_idx[:n] if dist[idx]<=threshold]
         return match_l
 
-    # No longer used    
-    def similarity(self, c1, c2):
-        v1 = self.transform(c1)
-        v2 = self.transform(c2)
-        return np.linalg.norm(v1-v2)
+    def similarity(self, v1, v2):
+        """Find distance between sparse vectors."""
+        return sp.sparse.linalg.norm(v1-v2)
 
